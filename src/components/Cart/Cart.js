917acd741ab3a2a0609100a63ededf4e8d5d91f8
mila2999 <mila.savonik@gmail.com>
2023-10-30 14:41:25 +0200

    Add the Modal via a React Portal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,15 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: c1, name: 'sushi', amount: 2, price: 12.99 }].map((item) => {
+      {[{ id: 'm1', name: 'sushi', amount: 2, price: 12.99 }].map((item) => {
         <li>{item.name}</li>;
       })}
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total amount</span>
@@ -18,7 +19,7 @@ const Cart = (props) => {
         <button className={classes['button--alt ']}>Close</button>
         <button className={classes.button}>Order </button>
       </div>
-    </div>
+    </Modal>
   );
 };
 export default Cart;
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css';
+const Backbrop = (props) => {
+  return <div className={classes.backbrop}></div>;
+};
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+const portalElement = document.getElementById('overlays');
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backbrop />, portalElement)}
+      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+    </Fragment>
+  );
+};
+export default Modal;
